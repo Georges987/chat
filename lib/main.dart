a9f00d7796e029c195edd5deccd12001c3f3a1a0
= <=>
2024-01-26 16:47:54 +0100

    removing debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ Future<void> main() async {
   runApp(
     GetMaterialApp(
       title: "Application",
+      debugShowCheckedModeBanner: false,
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
